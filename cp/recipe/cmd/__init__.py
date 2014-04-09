52da85a05752e8d278c9ed43c405bfe86cd17d90
Marco Mariani <marco.mariani@nexedi.com>
2014-04-09 10:58:23 +0200

    PEP8 tabs: hard -> soft

--- cp/recipe/cmd/__init__.py
+++ cp/recipe/cmd/__init__.py
@@ -8,7 +8,7 @@
 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
@@ -22,85 +22,85 @@ import os, sys
 
 
 class CmdExecutionFailed(Exception):
-	pass
+    pass
 
 
 class Cmd(object):
-	"""This recipe is used by zc.buildout"""
+    """This recipe is used by zc.buildout"""
 
-	def __init__(self, buildout, name, options):
-		self.buildout, self.name, self.options = buildout, name, options
-		self.on_install = options.get('on-install', True)
-		self.on_update = options.get('on-update', True)
-		self.shell = options.get('shell','/bin/sh')
+    def __init__(self, buildout, name, options):
+        self.buildout, self.name, self.options = buildout, name, options
+        self.on_install = options.get('on-install', True)
+        self.on_update = options.get('on-update', True)
+        self.shell = options.get('shell','/bin/sh')
 
-	def install(self):
-		"""installer"""
-		cmds = self.options.get('install-cmd', '')
-		if self.on_install:
-			self.execute(cmds)
-		return tuple()
+    def install(self):
+        """installer"""
+        cmds = self.options.get('install-cmd', '')
+        if self.on_install:
+            self.execute(cmds)
+        return tuple()
 
-	def update(self):
-		"""updater"""
-		cmds = self.options.get('update-cmd', '')
-		if self.on_update:
-			self.execute(cmds)
-		return tuple()
+    def update(self):
+        """updater"""
+        cmds = self.options.get('update-cmd', '')
+        if self.on_update:
+            self.execute(cmds)
+        return tuple()
 
-	def execute(self,cmds):
-		"""run the commands
-		"""
-		cmds = cmds.strip()
-		if not cmds:
-			return
-		if cmds:
-			cmds = cmds.split('\n')
-			dirname = tempfile.mkdtemp()
-			lines = [line.strip() for line in cmds]
-			tmpfile = os.path.join(dirname,'run.sh')
-			fil = open(tmpfile,'w+')
-			fil.write("#!%s\n" % self.shell)
-			fil.write('\n'.join(lines))
-			fil.close()
-			#give execute permissions
-			os.chmod(tmpfile,0o700)
-			process = subprocess.Popen([tmpfile], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0)
-			while True:
-				line = process.stdout.readline()
-				if not line:
-					break
-				sys.stdout.write(line)
-				sys.stdout.flush()
-			process.wait()
-			status = process.returncode
-			if status:
-				raise CmdExecutionFailed('while running %s, see the output above.' % tmpfile)
-			return status
+    def execute(self,cmds):
+        """run the commands
+        """
+        cmds = cmds.strip()
+        if not cmds:
+            return
+        if cmds:
+            cmds = cmds.split('\n')
+            dirname = tempfile.mkdtemp()
+            lines = [line.strip() for line in cmds]
+            tmpfile = os.path.join(dirname,'run.sh')
+            fil = open(tmpfile,'w+')
+            fil.write("#!%s\n" % self.shell)
+            fil.write('\n'.join(lines))
+            fil.close()
+            #give execute permissions
+            os.chmod(tmpfile,0o700)
+            process = subprocess.Popen([tmpfile], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=0)
+            while True:
+                line = process.stdout.readline()
+                if not line:
+                    break
+                sys.stdout.write(line)
+                sys.stdout.flush()
+            process.wait()
+            status = process.returncode
+            if status:
+                raise CmdExecutionFailed('while running %s, see the output above.' % tmpfile)
+            return status
 
 
 class Python(Cmd):
 
-	def execute(self):
-		"""run python code
-		"""
-		cmds = self.options.get('cmds', '')
-		cmds = cmds.strip()
-		def undoc(l):
-			l = l.strip()
-			l = l.replace('>>> ', '')
-			l = l.replace('... ', '')
-			return l
+    def execute(self):
+        """run python code
+        """
+        cmds = self.options.get('cmds', '')
+        cmds = cmds.strip()
+        def undoc(l):
+            l = l.strip()
+            l = l.replace('>>> ', '')
+            l = l.replace('... ', '')
+            return l
 
-		if not cmds:
-			return
-		if cmds:
-			lines = cmds.split('\n')
-			lines = [undoc(line) for line in lines if line.strip()]
-			dirname = tempfile.mkdtemp()
-			tmpfile = os.path.join(dirname, 'run.py')
-			open(tmpfile, 'w').write('\n'.join(lines))
-			exec(compile(open(tmpfile).read(), tmpfile, 'exec'))
-			shutil.rmtree(dirname)
+        if not cmds:
+            return
+        if cmds:
+            lines = cmds.split('\n')
+            lines = [undoc(line) for line in lines if line.strip()]
+            dirname = tempfile.mkdtemp()
+            tmpfile = os.path.join(dirname, 'run.py')
+            open(tmpfile, 'w').write('\n'.join(lines))
+            exec(compile(open(tmpfile).read(), tmpfile, 'exec'))
+            shutil.rmtree(dirname)
 
 
